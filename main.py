885217557533ee300e0b72a920e43a0cc1cd5edb
sinano1107 <tftf161107@icloud.com>
2023-02-11 21:10:15 +0900

    [add] main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from src.generate_tetrahedron import generateTetrahedron
+
+app = FastAPI()
+
+
+@app.get('/')
+def root():
+    positions, normals = generateTetrahedron()
+    return {
+        'positions': [p.tolist() for p in positions],
+        'normals': [n.tolist() for n in normals]
+    }
