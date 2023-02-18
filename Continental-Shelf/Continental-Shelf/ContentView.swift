9fd9ca0355228cdfbb6d1643b2eadf7d4a2fbfd8
sinano1107 <tftf161107@icloud.com>
2023-02-19 03:36:39 +0900

    [update] サーバーを叩いて四面体を表示

--- Continental-Shelf/Continental-Shelf/ContentView.swift
+++ Continental-Shelf/Continental-Shelf/ContentView.swift
@@ -6,16 +6,57 @@
 //
 
 import SwiftUI
+import simd
+import RealityKit
+import SceneKit
+
+struct CoralData: Codable {
+    var positions: [simd_float3]
+    var normals: [simd_float3]
+    
+    func build() -> ModelEntity {
+        var descr = MeshDescriptor()
+        descr.positions = MeshBuffers.Positions(positions)
+        descr.normals = MeshBuffers.Normals(normals)
+        descr.primitives = .triangles([UInt32](0...12))
+        let material = SimpleMaterial()
+        let model = ModelEntity(mesh: try! .generate(from: [descr]), materials: [material])
+        return model
+    }
+}
 
 struct ContentView: View {
+    @State var model = ModelEntity(mesh: .generateBox(size: 1), materials: [SimpleMaterial()])
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            OrbitView(entity: model, firstRadius: 6)
+                .ignoresSafeArea()
+                .toolbar {
+                    ToolbarItem(placement: .bottomBar) {
+                        Button(action: getData) {
+                            Image(systemName: "arrow.clockwise")
+                                .font(.largeTitle)
+                        }
+                    }
+                }
         }
-        .padding()
+    }
+    
+    func getData() {
+        guard let url = URL(string: "http://localhost:8000/") else { return }
+        URLSession.shared.dataTask(with: url) {(data, response, error) in
+            do {
+                if let data = data {
+                    let decodedData = try JSONDecoder().decode(CoralData.self, from: data)
+                    model = decodedData.build()
+                } else {
+                    print("No data", data as Any)
+                }
+            } catch {
+                print("Error", error)
+            }
+        }.resume()
     }
 }
 
